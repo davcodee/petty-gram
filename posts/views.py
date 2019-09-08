2732a1c7d55b2cb123f1e22f4872068fa3caf51c
David Morales <david_ivan240@ciencias.unam.mx>
2019-09-08 01:17:08 -0500

    Se agregó bootstrap y se dió estilos a la cards

--- posts/views.py
+++ posts/views.py
@@ -1,40 +1,43 @@
 """Posts Views"""
-from django.http import HttpResponse
+from django.shortcuts import render
 
 #utilities
 from datetime import datetime
+
+
 posts = [
     {
-        'name': 'My dog',
-        'user': 'David Morales',
+        'title': 'My dog',
+        'user': {
+            'name': 'David Morales',
+            'picture':'https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/237/200/300'
+        'photo': 'https://picsum.photos/id/237/200/300'
     },
     {
-        'name': 'My dog',
-        'user': 'David Morales',
+        'title': 'Miztli',
+        'user':{
+            'name': 'Isaac Ulises',
+            'picture': 'https://avatars3.githubusercontent.com/u/28477116?s=400&u=76e53b6a61862ac52245edce97ad33f47490cab1&v=4'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/237/200/300'
+        'photo': 'https://picsum.photos/id/237/200/300'
     },
     {
-        'name': 'My dog',
-        'user': 'David Morales',
+        'title': 'Percefone',
+        'user': {
+            'name': 'Anita michelle',
+            'picture': 'https://avatars3.githubusercontent.com/u/28477116?s=400&u=76e53b6a61862ac52245edce97ad33f47490cab1&v=4'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/id/237/200/300'
+        'photo': 'https://picsum.photos/id/237/200/300'
     },
 
 ]
 
 def list_posts(request):
-    """List existing posts"""
-    content = []
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i> {timestamp}</small></p>
-            <p><strong>{name}</strong></p>
-            <figure><img src="{picture}"></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
-
+    return render(request,'feed.html', {
+        'posts': posts
+    })
 
