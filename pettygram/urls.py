fe7fc12f892b486fafaa9872788e4ed11a641ab9
David Morales <david_ivan240@ciencias.unam.mx>
2019-10-20 20:40:24 -0500

    Se agrego el modelo post y se modico el media para subir archivos staticos

--- pettygram/urls.py
+++ pettygram/urls.py
@@ -4,7 +4,11 @@ Modulo de URLS de pettygram
 """
 #modulo de django
 from django.urls import path
+from django.conf import settings
+
 from django.contrib import admin
+from django.conf.urls.static import static
+
 # modulo de pettygram
 from pettygram  import views as local_views
 from posts  import  views as posts_views
@@ -17,4 +21,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
     path('posts/',posts_views.list_posts)
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/models.py
+++ posts/models.py
@@ -1,2 +1,21 @@
+"""Posts models"""
 
+
+#Django
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    """Post Model """
+
+    user = models.ForeignKey(User,on_delete= models.CASCADE) 
+    profile = models.ForeignKey('users.Profile', on_delete = models.CASCADE)
+    
+    title = models.CharField(max_length= 250)
+    photo = models.ImageField(upload_to="posts/photos")
    
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return "{} by @{}".format(self.title, self.user.username)
